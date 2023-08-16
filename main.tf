5c801cb8f1d48c38d7916df7dcf3bc0ed6095b81
Gopinaath C <37203318+gopinaathct@users.noreply.github.com>
2023-08-16 21:04:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,17 @@ resource "aws_s3_bucket" "example" {
 terraform {
   cloud {
     organization = "ctgopinaath789"
-
     workspaces {
-      name = "terraform_test"
+      name = "dev"
     }
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }  
+}
+provider "aws" {
+  region = "us-east-1"
 }
